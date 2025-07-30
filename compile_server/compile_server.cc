0c7153cc1fa2128823b515bad81866c70859b16c
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-30 17:05:03 +0800

    Update compile_server.cc

--- compile_server/compile_server.cc
+++ compile_server/compile_server.cc
@@ -1,11 +1,15 @@
 #include "compiler.hpp"
+#include "runner.hpp"
 
 using namespace cx_compiler;
+using namespace cx_runner;
 
 int main()
 {
     std::string code = "code";
+    //编译：
     Compiler::Compile(code);
-
+    //运行：
+    Runner::Run(code);
     return 0;
 }
