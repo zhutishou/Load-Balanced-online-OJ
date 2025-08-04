cf1195c4ddef65250afb2c2d0811d2b774f84df9
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-08-04 21:17:29 +0800

    Create header.cpp

---
+++ oj_server/questions/1/header.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+//该文件是用来存放用户编写后的内容
+
+//实际使用中：我们利用提前写好的宏将header.cpp和tail.cpp文件整合在一起提交给编译运行服务器compile_server.cc然后调用compile_run.hpp
+
+using namespace std;
+
+class Solution
+{
+    public:
+        bool isPalindrome(int x)
+        {
+            //code
+            return true;
+        }
+};
