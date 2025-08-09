0b0bfe9cda5a5bd1fcaabe1dd759d99ef7327881
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-08-09 10:47:02 +0800

    Update oj_server.cc

--- oj_server/oj_server.cc
+++ oj_server/oj_server.cc
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <signal.h>
 
 #include "../comm/httplib.h"
 #include "oj_control.hpp"
 
 
-
+using namespace cx_control;
 using namespace httplib;
 
 
@@ -16,23 +17,28 @@ int main()
     Control ctrl;
 
     //1.获取题目列表
-    svr.Get("/all_questions",[](const Request& req,Response& resp){
+    svr.Get("/all_questions",[&ctrl](const Request& req,Response& resp){
         std::string html;
         ctrl.AllQuestions(&html);
         resp.set_content(html,"text/html;charset = utf-8");
     });
     //2.根据题目编号获取题目内容
     // /question/id 正则表达式
-    svr.Get(R"(/question/(\d+))",[](const Request& req,Response& resp){
+    svr.Get(R"(/question/(\d+))",[&ctrl](const Request& req,Response& resp){
         std::string number = req.matches[1];//id
         std::string html;
         ctrl.Questions(number,&html);
         resp.set_content(html,"text/html;charset = utf-8");
     });
     //3.根据用户提交的代码进行判题
-    svr.Get(R"(/judge/(\d+))",[](const Request& req,Response& resp){
+    svr.Post(R"(/judge/(\d+))",[&ctrl](const Request& req,Response& resp){
         std::string number = req.matches[1];//id
-        resp.set_content("判断指定的题目: " + number,"text/plain;charset = utf-8");
+        std::string result_json;
+        ctrl.Judge(number,req.body,&result_json);
+        resp.set_content(result_json,"application/json;charset = utf-8");
+
+        // std::string number = req.matches[1];//id
+        // resp.set_content("判断指定的题目: " + number,"text/plain;charset = utf-8");
     });
     //设置路径
     svr.set_base_dir("./wwwroot");
