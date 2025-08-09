385ad0e903d722a5ae61d1585946c1161db07860
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-08-09 10:46:37 +0800

    Update oj_control.hpp

--- oj_server/oj_control.hpp
+++ oj_server/oj_control.hpp
@@ -4,11 +4,13 @@
 #include <string>
 #include <mutex>
 #include <cassert>
+#include <jsoncpp/json/json.h>
 
 #include "oj_model.hpp"
 #include "oj_view.hpp"
 #include "../comm/util.hpp"
 #include "../comm/log.hpp"
+#include "../comm/httplib.h"
 
 //用于处理核心业务
 
@@ -19,6 +21,7 @@ namespace cx_control
     using namespace cx_util;
     using namespace cx_model;
     using namespace cx_view;
+    using namespace httplib;
 
     //机器类
     class Machine
@@ -66,6 +69,7 @@ namespace cx_control
             _load = load;
             //解锁
             _mtx->unlock();
+
             return _load;
         }
     };
@@ -174,11 +178,32 @@ namespace cx_control
         }
         void OnlineMachine()
         {
-            //
+            //后期添加
         }
+        //only for test
         void ShowMachines()
         {
-            //
+            //上锁
+            mtx.lock();
+            //显示上线的主机情况
+            std::cout<<"当前上线的主机列表: ";
+            for(auto id : online)
+            {
+                //显示输出id情况
+                std::cout<<id<<" ";
+            }
+            std::cout<<std::endl;
+            //显示离线的主机情况
+            std::cout<<"当前离线的主机列表: ";
+            for(auto id : offline)
+            {
+                //显示输出id情况
+                std::cout<<id<<" ";
+            }
+            std::cout<<std::endl;
+
+            //解锁
+            mtx.unlock();
         }
     private:
         std::vector<Machine> machines;//所有可以参与编译运行服务主机
@@ -186,12 +211,6 @@ namespace cx_control
         std::vector<int> offline;//所有离线主机ID
         std::mutex mtx;//定义锁
     };
-
-
-
-
-
-
 
     //核心控制器
     class Control
@@ -202,7 +221,7 @@ namespace cx_control
         //根据题目数据构建网页
         bool AllQuestions(std::string* html)
         {
-            vector<struct Question> all;
+            std::vector<struct Question> all;
             //利用Model类函数
             if(_model.GetAllQuestions(&all))
             {
@@ -235,11 +254,69 @@ namespace cx_control
         //判断用户编写的代码正确与否功能
         void Judge(const std::string number,const std::string in_json,std::string* out_json)
         {
+            //1.根据题目编号找到题目细节部分
+            struct Question q;
+            _model.GetOneQuestion(number,&q);
 
+            //2.将in_json进行反序列化，得到用户提交的源码和题目id
+            Json::Reader reader;
+            Json::Value in_value;
+            reader.parse(in_json,in_value);
+            std::string code = in_value["code"].asString();
+
+            //3.重新拼接用户代码和测试用例代码形成新的代码
+            Json::Value compile_value;
+            compile_value["input"] = in_value["input"].asString();
+            compile_value["code"] = code + q.tail;
+            compile_value["cpu_limit"] = q.cpu_limit;
+            compile_value["mem_limit"] = q.mem_limit;
+            Json::FastWriter writer;
+            std::string compile_string = writer.write(compile_value);
+
+            //4.选择负载情况最低的主机
+            while(true)
+            {
+                int id;
+                Machine* m = nullptr;
+                if(!_load_blance.SmartChoice(&id,&m))
+                {
+                    //选择失败
+                    break;
+                }
+                //选择成功，打印日志
+                LOG(INFO)<<"选择主机成功,主机id: "<< id << "详细: " << m->ip << " : "<<m->port << "\n";
+                
+                //4.发起http请求,得到结果
+                Client cli(m->ip,m->port);
+                m->IncLoad();
+                if(auto res = cli.Post("/compile_and_run",compile_string,"application/json;charset=utf-8"))
+                {
+                    //5.将结果赋值给out_json
+                    if(res->status == 200)
+                    {
+                        //200表示传输成功
+                        *out_json = res->body;
+                        m->DecLoad();
+                        LOG(INFO)<<"请求编译和运行成功 "<<"\n";
+                        break;
+                    }
+                    m->DecLoad();
+                }
+                else
+                {
+                    //失败
+                    //打印日志
+                    LOG(ERROR)<<"当前请求的主机id: "<< id << "详细:" << m->ip <<":"<<m->port<<"可能已经离线"<<"\n";
+                    _load_blance.OfflineMachine(id);
+                    //only for test 打印观察在线主机和离线主机
+                    _load_blance.ShowMachines();
+                }
+
+            }
         }
     private:
-        Model _model;//提供后台数据
-        View _view;//提供html渲染功能
-        LoadBalance _load_blance; //核心负载均衡器
+        Model _model;               //提供后台数据
+        View _view;                 //提供html渲染功能
+        LoadBalance _load_blance;   //核心负载均衡器
     };
 }
